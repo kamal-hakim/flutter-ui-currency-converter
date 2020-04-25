5dd3da7e45fc23c83643a8f7432d3518ac4ba30d
Kamal Hakim <kamalhakim.zainal@gmail.com>
2020-04-25 11:36:18 +0800

    completed basic dashboard UI without logic

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -15,26 +15,147 @@ class Dashboard extends StatelessWidget {
           height: MediaQuery.of(context).size.height / 2,
         ),
         Container(
-          child: Center(
-            child: Container(
-              height: 120.0,
-              width: 120.0,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(60.0),
-                border: Border.all(
-                  color: Theme.of(context).primaryColor,
-                  width: 7.0,
-                ),
-                color: Colors.white,
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.arrow_downward,
-                  size: 70.0,
-                  color: Theme.of(context).primaryColor,
+          child: Column(children: <Widget>[
+            Expanded(
+              child: SafeArea(
+                child: Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        "Euro",
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "200.0",
+                              style: TextStyle(
+                                fontSize: 100.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 10.0),
+                            Baseline(
+                              baseline: 70,
+                              baselineType: TextBaseline.alphabetic,
+                              child: Text(
+                                "EUR",
+                                style: TextStyle(
+                                  fontSize: 30.0,
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
+            Expanded(
+              child: Container(
+                child: Stack(
+                  children: <Widget>[
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "116.77",
+                            style: TextStyle(
+                              fontSize: 100.0,
+                              color: Theme.of(context).accentColor,
+                            ),
+                          ),
+                          SizedBox(width: 10.0),
+                          Baseline(
+                            baseline: 70,
+                            baselineType: TextBaseline.alphabetic,
+                            child: Text(
+                              "\$",
+                              style: TextStyle(
+                                fontSize: 30.0,
+                                color: Theme.of(context).accentColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          bottom: 40.0,
+                        ),
+                        child: Text(
+                          "United States Dollar",
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            color: Theme.of(context).accentColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ]),
+        ),
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "EUR",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.grey[400],
+                ),
+              ),
+              SizedBox(height: 25.0),
+              Container(
+                height: 120.0,
+                width: 120.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(60.0),
+                  border: Border.all(
+                    color: Theme.of(context).primaryColor,
+                    width: 7.0,
+                  ),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.arrow_downward,
+                    size: 70.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0),
+              Text(
+                "USD",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+            ],
           ),
         )
       ],
